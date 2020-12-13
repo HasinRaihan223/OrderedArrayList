242650e6f7591a2dbc21947376b5718cc209f743
HasinRaihan223 <hraihan20@stuy.edu>
2020-12-13 11:34:15 -0500

    Finished all methods for this class

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,12 +1,39 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
+
   NoNullArrayList(){
     super();
   }
 
-  NoNullArrayList(int initialCapacity){
-    super(initialCapacity);
+  NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
   }
 
+  public T set(int index, T t){
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be " + t);
+    }
+    else {
+      return super.set(index, t);
+    }
+  }
+
+  public boolean add(T t){
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be " + t);
+    }
+    else {
+      return super.add(t);
+    }
+  }
+
+  public void add(int index, T t){
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be " + t);
+    }
+    else {
+      super.add(index, t);
+    }
+  }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # OrderedArrayList
-Our breakout group discussed some subtle nuances about the lab and some of the submission guidelines. Some people were confused about what the "edit readme.md file" actually meant. A lot of us were confused about how exactly implement the constructors, so we had a little discussion about that and thought we should just call super for their initialization. 
+Our breakout group discussed some subtle nuances about the lab and some of the submission guidelines. Some people were confused about what the "edit readme.md file" actually meant (so we cleared that up). A lot of us were confused about how exactly implement the constructors, so we had a little discussion about that and thought we should just call super for their initialization. 
